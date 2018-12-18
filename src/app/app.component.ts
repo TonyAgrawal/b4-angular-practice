2bdd2e3368ce5bbbfb0323238a901369e11f79cc
scmpadiyala <sridhar.padiyala@gmail.com>
2018-12-18 07:57:34 +0530

    First project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'b4-angular-practice';
-  first angular project
-
-
-/* 
-  * Sample Test Program
-  */
-
-  /*
-  This is my test comments
-  Welcome to test page
-  */
+  title = 'b4-angular-practice1';
 }
